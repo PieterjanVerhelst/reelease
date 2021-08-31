7a569da541bc308b54e5cf294413364300b0f824
Pieterjan Verhelst <pieterjan.verhelst@inbo.be>
2021-08-31 14:35:57 +0200

    conduct mann whitney u test

--- src/speed_analysis.R
+++ src/speed_analysis.R
@@ -85,3 +85,35 @@ fig_migrationspeed
 
 
 
+
+# independent 2-group t-test
+# t.test(y~x) # where y is numeric and x is a binary factor 
+t.test(total_tracking$total_speed_km_day~total_tracking$dst, var.equal = TRUE)
+
+# Assumptions:
+# 1. 2 independent groups
+# N and SW are independent (each direction is attributed to a different animal)
+
+# 2. Normality
+qqnorm(total_tracking$total_speed_km_day, pch = 1, frame = TRUE)
+qqline(total_tracking$total_speed_km_day, col = "steelblue", lwd = 2)
+
+shapiro.test(total_tracking$total_speed_km_day)
+# The p-value > 0.05 implying that the distribution of the data are not significantly different from normal distribution. In other words, we can assume the normality.
+
+# 3. Homogeneity of variances
+# F-test 
+# The F-test is used to assess whether the variances of two populations (A and B) are equal. The test requires normality (in case no normality, apply Fligner-Killeen’s test)
+# https://www.datanovia.com/en/lessons/homogeneity-of-variance-test-in-r/
+var.test(total_speed_km_day ~ dst, data = total_tracking)
+# When p > 0.05, there is no significant difference between the two variances.
+
+
+# Mann-Whitney U test as non-parametric alternative for the independent 2-group t-test
+# https://www.statmethods.net/stats/nonparametric.html
+wilcox.test(total_speed_km_day ~ dst, data = total_tracking)
+
+
+
+
+
