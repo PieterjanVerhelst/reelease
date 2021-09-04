0b44cde2a46e56773d9218fb3000c9e7bfa3ddcf
Pieterjan Verhelst <pieterjan.verhelst@inbo.be>
2021-09-04 15:53:19 +0200

    analyse release residence time

--- src/release_analysis.R
+++ src/release_analysis.R
@@ -45,4 +45,94 @@ eel$animal_project_code <- "Reelease"
 data2 <- rbind(eel, data2)
 
 
+# Summarise max tracking time ####
+residence <- data2 %>%
+  group_by(tag_id) %>%
+  summarise(total_time = max(date_time) - min(date_time))
+
+residence$total_time <- as.numeric(residence$total_time/3600)
+
+
+# Add column with dst ####
+# Load eel metadata
+eel <- read_csv("./data/raw/eel_metadata.csv")
+eel <- eel %>%
+  select(transmitter, length_mm, weight_g, dst) %>%
+  rename(tag_id = transmitter)
+
+# Merge eel data to detection data
+residence <- left_join(residence, eel, by = "tag_id")
+residence$dst <- factor(residence$dst)
+
+
+# Calculate summaries ####
+aggregate(residence$total_time, list(residence$dst), median)
+
+# Create simple boxplot of speed
+boxplot(total_time~dst, residence,
+        xlab = "DST",
+        ylab = "Residence time (h)",
+        ylim=c(0, 30), cex.lab=1.25, cex.axis=1.25)
+
+# Create elaborated boxplot with number of eels per class
+# make a named list for the location of the number of eels
+eel_per_class <- residence %>% group_by(dst) %>% 
+  summarise(n_eels = n_distinct(tag_id))
+eels_per_class_list <- rep(30, nrow(eel_per_class))
+names(eels_per_class_list) <- as.vector(eel_per_class$dst)
+# create ggplot (cfr. styling earlier plot)
+fig_residence <- ggplot(residence, aes(x = dst,
+                                                 y = total_time)) +
+  geom_boxplot() +
+  scale_y_continuous(breaks = seq(0, 30, by = 5), limits=c(0,30)) +
+  theme_minimal() +
+  ylab("Residence time (h)") +
+  geom_text(data = data.frame(),
+            aes(x = names(eels_per_class_list),
+                y = eels_per_class_list,
+                label = as.character(eel_per_class$n_eels)),
+            col = 'black', size = 4) +
+  scale_x_discrete(limits=c("0","1")) +    # Changes oreder of plots
+  xlab("DST") +
+  theme(axis.title.y = element_text(margin = margin(r = 10))) +
+  theme(axis.text = element_text(size = 12),
+        axis.title = element_text(size = 14))
+fig_residence
+
+
+
+
+# independent 2-group t-test
+# t.test(y~x) # where y is numeric and x is a binary factor 
+t.test(residence$total_time~residence$dst, var.equal = TRUE)
+
+# Assumptions:
+# 1. 2 independent groups
+# N and SW are independent (each direction is attributed to a different animal)
+
+# 2. Normality
+qqnorm(residence$total_time, pch = 1, frame = TRUE)
+qqline(residence$total_time, col = "steelblue", lwd = 2)
+
+shapiro.test(residence$total_time)
+# The p-value > 0.05 implying that the distribution of the data are not significantly different from normal distribution. In other words, we can assume the normality.
+
+# 3. Homogeneity of variances
+# F-test 
+# The F-test is used to assess whether the variances of two populations (A and B) are equal. The test requires normality (in case no normality, apply Fligner-Killeen’s test)
+# https://www.datanovia.com/en/lessons/homogeneity-of-variance-test-in-r/
+var.test(total_time ~ dst, data = residence)
+# When p > 0.05, there is no significant difference between the two variances.
+
+
+# Mann-Whitney U test as non-parametric alternative for the independent 2-group t-test
+# https://www.statmethods.net/stats/nonparametric.html
+wilcox.test(total_time ~ dst, data = residence)
+
+
+
+
+
+
+
 
